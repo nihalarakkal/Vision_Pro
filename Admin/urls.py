ec0fc198ae6111f18da163a1c2b0dd7036d7cc51
nihalarakkal <nihalarakkal578@gmail.com>
2025-03-16 15:26:35 +0530

    "final commit"

--- Admin/urls.py
+++ Admin/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
 from Admin.views import homeTemplateView
 from Admin import views
+from .views import order_list,user_list
 
 
 app_name = "Admin"
 
 urlpatterns= [
              path('', homeTemplateView.as_view(), name="admin"),
+             path('login_page/',views.login_page,name="login_pages"),
+             path('admin_login/',views.admin_login,name="Admin_login"),
+             path('admin_logout/',views.admin_logout,name="Admin_logout"),
+             path('product_page/',views.product_page,name="admin_products"),
+             path('save_products/',views.admin_save_products,name="admin_save_products"),
+             path('edit_products/<int:prd_id>/',views.admin_edit_products,name="admin_edit_products"),
+             path('update_products/<int:prd_id>/',views.admin_update_products,name="admin_update_products"),
+             path('delete_products/<int:prd_id>/',views.admin_delete_products,name="admin_delete_products"),
+             path('display_products/',views.display_products,name='admin_display_products'),
+             path("users/", user_list, name="user_list"),
+             path('orderd/',order_list,name="order_list")
+
 ]
--- Admin/views.py
+++ Admin/views.py
@@ -1,8 +1,105 @@
 from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
-from core.models import Item
-
+from core.models import Item,UserProfile,OrderItem,Order
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.core.files.storage import FileSystemStorage
+from django.utils.datastructures import MultiValueDictKeyError
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 # Create your views here.
 
 class homeTemplateView(TemplateView):
     template_name="admin_home.html"
+def login_page(request):
+    return render(request,"login.html")
+def admin_login(request):
+    if request.method=="POST":
+        un = request.POST.get('username')
+        pwd = request.POST.get('pass')
+        if User.objects.filter(username__contains=un).exists():
+            x = authenticate(username=un, password=pwd)
+            if x is not None:
+              login(request, x)
+              request.session['username']=un
+              request.session['password']=pwd
+              messages.success(request,"Welcome")
+              return redirect("Admin:admin")
+        else:
+            messages.error(request,"Invalid Password")
+            return redirect("Admin:login_page")
+    else:
+        messages.warning(request,"user not found")
+        return redirect("Admin:login_page")
+def admin_logout(request):
+    del request.session['username']
+    del request.session['password']
+    messages.success(request,"Logout Successfully")
+    return redirect("Admin:login_page")
+def product_page(request):
+    catg=Item.objects.values_list('category', flat=True).distinct()
+    labe=Item.objects.values_list('label',flat=True).distinct()
+    return render(request,"product.html",{'catg':catg,'labe':labe})
+def admin_save_products(request):
+    if request.method=="POST":
+        ti=request.POST.get('title')
+        pr=request.POST.get('price')
+        dp=request.POST.get('d_price')
+        cat=request.POST.get('category')
+        lab=request.POST.get('label')
+        slg=request.POST.get('slug')
+        des=request.POST.get('description')
+        fea=request.POST.get('Features')
+        fv=request.FILES['img1']
+        sv=request.FILES['img2']
+    obj=Item(title=ti,price=pr,discount_price=dp,category=cat,label=lab,slug=slg,description=des, features=fea,fview=fv,sview=sv)
+    obj.save()
+    return redirect("Admin:display_products")
+def admin_edit_products(request,prd_id):
+    data=Item.objects.get(id=prd_id)
+    cat =Item.objects.all()
+    return render(request,"edit_products.html",{'data':data,'cat':cat})
+def admin_update_products(request,prd_id):
+    if request.method=="POST":
+        ti=request.POST.get('title')
+        pr=request.POST.get('price')
+        dp=request.POST.get('d_price')
+        cat=request.POST.get('category')
+        lab=request.POST.get('label')
+        slg=request.POST.get('slug')
+        des=request.POST.get('description')
+        fea=request.POST.get('Features')
+    try:
+        img=request.FILES['image']
+        fs=FileSystemStorage()
+        file=fs.save(img.name, img)
+    except MultiValueDictKeyError:
+        file=Item.objects.get(id=prd_id).fview
+        file=Item.objects.get(id=prd_id).sview
+        Item.objects.filter(id=prd_id).update(title=ti,price=pr,discount_price=dp,category=cat,label=lab,slug=slg,description=des, features=fea,fview=file,sview=file)
+    return redirect("Admin:display_products")
+
+def admin_delete_products(request,prd_id):
+  x=Item.objects.filter(id=prd_id)
+  x.delete()
+  return redirect("Admin:display_products")
+def display_products(request):
+    Products=Item.objects.all()
+    return render(request,"display_products.html",{'Products':Products})
+def user_list(request):
+    users = User.objects.all()  # Fetch all users
+    # Optimized query
+
+    context = {
+        "users": users,
+      
+    }
+    return render(request, "user.html", context)
+def order_list(request):
+    orders = Order.objects.select_related("user").prefetch_related("items") 
+    context = {
+        
+        "orders": orders,
+    }
+    return render(request,"orders.html",context)
